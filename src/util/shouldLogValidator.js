11798d58a303601189433d8d56ba89b728d6c4d0
Peter Han <peter.han.9493@gmail.com>
2020-02-23 18:33:59 -0500

    Add safeguard for only guild text channels

--- src/util/shouldLogValidator.js
+++ src/util/shouldLogValidator.js
@@ -3,6 +3,8 @@ const { guildChannelMap } = require('../config');
 module.exports = message => {
   if (message.author.bot) return false;
 
+  if (message.channel.type !== 'text') return false;
+
   const guildConfiguration = guildChannelMap[message.channel.guild.id];
   if (!guildConfiguration) return false;
 
