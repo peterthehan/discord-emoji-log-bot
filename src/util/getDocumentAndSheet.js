eea733591b09d49baf7b16e51d301995f35cccee
Peter Han <peter.han.9493@gmail.com>
2020-02-23 00:50:42 -0500

    Add doc cache

--- src/util/getDocumentAndSheet.js
+++ src/util/getDocumentAndSheet.js
@@ -1,12 +1,19 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { credentials, sheetId } = require('../config');
 
-module.exports = async title => {
-  const document = new GoogleSpreadsheet(sheetId);
-  await document.useServiceAccountAuth(credentials);
-  await document.loadInfo();
-  if (!document) return {};
+const documentCache = {};
 
+module.exports = async title => {
+  if (!(sheetId in documentCache)) {
+    const document = new GoogleSpreadsheet(sheetId);
+    await document.useServiceAccountAuth(credentials);
+    await document.loadInfo();
+    if (!document) return {};
+
+    documentCache[sheetId] = document;
+  }
+
+  const document = documentCache[sheetId];
   const sheet = document.sheetsByIndex.find(sheet => sheet.title === title);
   if (!sheet) return { document };
 
