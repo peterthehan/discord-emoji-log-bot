c1ac957ca1ad3def79f465a5f3e952c96af06096
Peter Han <peter.han.9493@gmail.com>
2020-03-08 22:48:20 -0400

    Change config to match readme for ease of use #3 (#5)

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
+const isProduction = 'TOKEN' in process.env;
+
 module.exports = {
-  ...('TOKEN' in process.env
+  credentials: isProduction
     ? {
-        sheetId: '1CrvJ3Zuec5F7dF5ez_nPOJ5auZVfPrXz_W8GG2o0PZM',
-        credentials: {
-          client_email: process.env.CLIENT_EMAIL,
-          private_key: process.env.PRIVATE_KEY
-        }
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY
       }
-    : {
-        sheetId: '1C0G4ijmfF7StZkCgpiv3paOraIh9k9h7GmjVVZUrZNM',
-        credentials: require('../../../credentials')
-      }),
+    : require('../../../credentials'),
+  sheetId: isProduction
+    ? '1CrvJ3Zuec5F7dF5ez_nPOJ5auZVfPrXz_W8GG2o0PZM'
+    : '1C0G4ijmfF7StZkCgpiv3paOraIh9k9h7GmjVVZUrZNM',
   guildChannelMap: {
     '258167954913361930': {
       channelsToIgnore: []
